d046c44be4bd1750164bebd23d2637604493c910
Mohamad Jawhar <mohamad.jaouhar98@gmail.com>
2023-12-22 01:08:11 -0500

    reinitialize web worker when component renders

--- src/components/PCHuntProject/IphoneScene/IphoneScene.tsx
+++ src/components/PCHuntProject/IphoneScene/IphoneScene.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-// App.jsx (main thread)
+import React, { useEffect, useMemo } from 'react'
 import { lazy } from 'react'
 import { Canvas } from '@react-three/offscreen'
 import { useInView } from 'react-intersection-observer'
@@ -9,13 +8,21 @@ import { useFrame } from '@react-three/fiber'
 // This will happen on systems where OffscreenCanvas is not supported
 const Scene = lazy(() => import('./Scene'))
 
-// This is the worker thread that will render the scene
-const worker = new Worker(new URL('./Worker.tsx', import.meta.url), { type: 'module' })
-
 const DisableRender = () => useFrame(() => null, 1000)
 
 const IphoneScene = () => {
+  // This is the worker thread that will render the scene
+  const worker = useMemo(() => new Worker(new URL('./Worker.tsx', import.meta.url), { type: 'module' }), [])
+
   const { ref, inView } = useInView()
+
+  useEffect(
+    () => {
+      return () => worker.terminate()
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  )
 
   return (
     <div ref={ref} style={{ position: 'relative', height: '100%' }}>
